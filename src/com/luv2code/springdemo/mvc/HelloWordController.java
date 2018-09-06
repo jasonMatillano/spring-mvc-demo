0f092a7130f8092ec4142f11e63abea0bb9426cb
jasonMatillano <jason.matillano@gmail.com>
2018-09-06 18:30:38 +0800

    working model

--- src/com/luv2code/springdemo/mvc/HelloWordController.java
+++ src/com/luv2code/springdemo/mvc/HelloWordController.java
@@ -1,6 +1,8 @@
 package com.luv2code.springdemo.mvc;
 
+import javax.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -15,6 +17,26 @@ public class HelloWordController {
 	// controller method to process the form
 	@RequestMapping("/processForm")
 	public String processForm() {
+		return "helloworld";
+	}
+	
+	// create a new controller method to read from data
+	// add data to model
+	@RequestMapping("/processFormTwo")
+	public String letsShoutDude(HttpServletRequest request, Model model) {
+		
+		// read the request parameter from html form
+		String theName = request.getParameter("studentName");
+		
+		// convert data to all caps
+		theName = theName.toUpperCase();
+		
+		// create message
+		String result = "Yo!" + theName;
+				
+		// add message to model 
+		model.addAttribute("message", result);
+		
 		return "helloworld";
 	}
 }
